a715575ca1d120548763b4b42f4a85990231df62
Kaan Eser <69899907+kaaneser@users.noreply.github.com>
2021-09-18 23:44:25 +0300

    MVC pattern modified.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const ejs = require('ejs');
-const path = require('path');
 
-const Post = require('./models/Post');
+const postController = require('./controllers/postController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -18,31 +19,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['GET', 'POST'],
+  })
+);
 
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {posts});
-});
+// Routes
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.addPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add_post');
-});
-
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post
-  });
-})
-
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/posts/edit/:id', pageController.getEditPage);
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
 
 const port = 3000;
 app.listen(port, () => {
